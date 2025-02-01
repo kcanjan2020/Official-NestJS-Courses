a33ad062c0c3f3f988879b868af1271a2b349ce0
Anjan KC <kcanjan2020@gmail.com>
2025-02-01 17:23:53 +0545

    Env Validation

--- authentication-and-authorization/src/app.module.ts
+++ authentication-and-authorization/src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [CoffeesModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().alphanum().required(),
+        DB_Name: Joi.string().required(),
+      }),
+    }),
+    CoffeesModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
